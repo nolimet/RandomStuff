e0db28cddd99731e2980446e70c3f78510a5d1a3
Nolimet <nolimet@stamdata.nl>
2014-05-06 20:41:15 +0200

    stuffs

---
+++ Assets/Scripts/util/RandomRot.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RandomRot : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        transform.rotation = Quaternion.Euler(360f * Random.value, 360f * Random.value, 360f * Random.value);
+	}
+}
---
+++ Assets/Scripts/util/RotateAround.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RotateAround : MonoBehaviour {
+
+    public Transform parent;
+
+    public Vector2 direction;
+    public float heightchange;
+
+	// Use this for initialization
+	void Start () {
+        if (parent != null)
+        {
+            transform.parent = parent;
+        }
+        direction = (Random.value/2+0.5f) * direction;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.LookAt(transform.parent.position);
+        float z = 0;
+        if(heightchange!=0)
+            z = Mathf.PingPong(Time.time*3,heightchange);
+        Vector3 dir = new Vector3(direction.x,direction.y,z);
+        transform.Translate(dir * Time.deltaTime);
+	}
+}
