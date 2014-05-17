0a05158ea0822fd823a6f28ad6744d123709edc4
Nolimet <nolimet@stamdata.nl>
2014-05-17 23:17:36 +0200

    added a sound controler and a playlist

--- Assets/Scripts/Audio/Visualiser.cs
+++ Assets/Scripts/Audio/Visualiser.cs
@@ -1,45 +1,47 @@
 ﻿using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
-public class Visualiser : MonoBehaviour
-{
 
-    AudioSource tests;
-    [Range (0.01f,100f)]
-    public float audioDrawScale = 1f;
-    [Range(16,1024)]
-    public int SpectrumSize;
-    public List<Transform> cubes = new List<Transform>();
-    // Use this for initialization
-    void Start()
+namespace Audio
+{
+    public class Visualiser : MonoBehaviour
     {
-        bool b = true; ;
-        for (int i = SpectrumSize; i > 0; i--)
+        [Range(0.01f, 100f)]
+        public float audioDrawScale = 1f;
+        [Range(16, 1024)]
+        public int SpectrumSize;
+        public List<Transform> cubes = new List<Transform>();
+        // Use this for initialization
+        void Start()
         {
-            GameObject cube = Instantiate(Resources.Load("SimpleCube"), new Vector3(0, 0, 0), Quaternion.identity) as GameObject;
-            cube.name = "cube" + i;
-            cube.transform.parent = transform;
-            b = !b;
-            cubes.Add(cube.GetComponent<Transform>());
-        }
-    }
-    void FixedUpdate()
-    {
-        float barWidth = 25.6f / SpectrumSize;
-        float[] spectrum = audio.GetSpectrumData(SpectrumSize, 0, FFTWindow.BlackmanHarris);
-        int i = 0;
-        float channelSize = 0f;
-        while (i < SpectrumSize)
-        {
-            channelSize = spectrum[i];
-            if (spectrum[i] * audioDrawScale > 12f)
+            bool b = true; ;
+            for (int i = SpectrumSize; i > 0; i--)
             {
-                channelSize = 12f / audioDrawScale;
+                GameObject cube = Instantiate(Resources.Load("SimpleCube"), new Vector3(0, 0, 0), Quaternion.identity) as GameObject;
+                cube.name = "cube" + i;
+                cube.transform.parent = transform;
+                b = !b;
+                cubes.Add(cube.GetComponent<Transform>());
             }
-         //   Debug.DrawLine(new Vector3(0,0,i/100f),new Vector3(0,spectrum[i]/audioDrawScale,i/100f),Color.yellow);
-            cubes[i].localScale = new Vector3(barWidth, channelSize * audioDrawScale + 0.01f, barWidth);
-            cubes[i].position = new Vector3(0, (channelSize * audioDrawScale) / 2f, i * barWidth);
-            i++;
+        }
+        void FixedUpdate()
+        {
+            float barWidth = 25.6f / SpectrumSize;
+            float[] spectrum = audio.GetSpectrumData(SpectrumSize, 0, FFTWindow.BlackmanHarris);
+            int i = 0;
+            float channelSize = 0f;
+            while (i < SpectrumSize)
+            {
+                channelSize = spectrum[i];
+                if (spectrum[i] * audioDrawScale > 12f)
+                {
+                    channelSize = 12f / audioDrawScale;
+                }
+                //   Debug.DrawLine(new Vector3(0,0,i/100f),new Vector3(0,spectrum[i]/audioDrawScale,i/100f),Color.yellow);
+                cubes[i].localScale = new Vector3(barWidth, channelSize * audioDrawScale + 0.01f, barWidth);
+                cubes[i].position = new Vector3(0, (channelSize * audioDrawScale) / 2f, i * barWidth);
+                i++;
+            }
         }
     }
 }
