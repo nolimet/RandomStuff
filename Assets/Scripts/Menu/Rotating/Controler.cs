bc9e152ec18ccd29145fd33ec75eb728175cef74
Nolimet <nolimet@stamdata.nl>
2014-05-19 15:14:37 +0200

    added roterymenu base

---
+++ Assets/Scripts/Menu/Rotating/Controler.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Menu
+{
+    namespace Rotating
+    {
+        public class Controler : MonoBehaviour
+        {
+            public static string menuTag = "Menu";
+            public float distfromCenter = 10f;
+            public List<MenuObject> Menus = new List<MenuObject>();
+            public List<MenuButton> Button = new List<MenuButton>();
+            void Start()
+            {
+                object[] allObjects = FindObjectsOfTypeAll(typeof(GameObject)) ;
+                foreach (GameObject go in allObjects)
+                {
+                    if (go.tag == menuTag)
+                    {
+                        Menus.Add(go.GetComponent<MenuObject>());
+                        go.transform.parent = transform;
+                    }
+                }
+                int i = 0;
+                int l = Menus.Count;
+                foreach (MenuObject mo in Menus)
+                {
+
+                    mo.SetPos(Quaternion.Euler(0, 360f / l * i, 0f), distfromCenter, i);
+                    i++;
+                }
+            }
+
+            void Update()
+            {
+                
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Menu/Rotating/MenuObject.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+namespace Menu
+{
+    namespace Rotating
+    {
+        public class MenuObject : MonoBehaviour
+        {
+            GameObject pivot;
+            public int IndexNumb;
+            public void SetPos(Quaternion rot, float distsCenter, int _IndexNumb)
+            {
+                pivot = new GameObject("Pivot for "+name);
+                pivot.transform.parent = transform.parent;
+                transform.parent = pivot.transform;
+                transform.localPosition = Vector3.zero;
+                pivot.transform.localPosition = Vector3.zero;
+                pivot.transform.rotation = rot;
+                transform.Translate(0, 0, distsCenter);
+                name=name + "[" +_IndexNumb +  "]";
+            }
+        }
+    }
+}
