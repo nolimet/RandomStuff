ae598437d821325602572ed29f3a98b5f1bfdab6
Nolimet <nolimet@stamdata.nl>
2014-06-14 14:10:26 +0200

    example

--- Assets/Example.cs
+++ Assets/Example.cs
@@ -3,13 +3,15 @@ using System.Collections;
 
 public class Example : MonoBehaviour
 {
-    public float heightScale = 1.0F;
-    public float xScale = 1.0F;
-    void Update()
+    public Vector3[] newVertices;
+    public Vector2[] newUV;
+    public int[] newTriangles;
+    void Start()
     {
-        float height = heightScale * Mathf.PerlinNoise(Time.time * xScale, 0.0F);
-        Vector3 pos = transform.position;
-        pos.y = height;
-        transform.position = pos;
+        Mesh mesh = new Mesh();
+        GetComponent<MeshFilter>().mesh = mesh;
+        mesh.vertices = newVertices;
+        mesh.uv = newUV;
+        mesh.triangles = newTriangles;
     }
 }
