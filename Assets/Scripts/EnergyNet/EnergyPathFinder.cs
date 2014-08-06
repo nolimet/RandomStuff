66a62cee31b9a3f5bc1ac16d4044948b9e5f0fe0
Nolimet <nolimet@stamdata.nl>
2014-08-06 23:49:43 +0200

    Began Work on Audiovisu

---
+++ Assets/Scripts/EnergyNet/EnergyPathFinder.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+namespace EnergyNet
+{
+    public class EnergyPathFinder : MonoBehaviour
+    {
+
+        Transform currentTarget;
+        EnergyBase origen;
+        List<EnergyBase> visted;
+
+        void Update()
+        {
+
+        }
+
+        void OnCollisionEnter(Collision col)
+        {
+            if (col.gameObject.tag == EnergyTags.EnergyNode)
+            {
+
+            }
+        }
+    }
+}
