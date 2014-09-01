59d4d7ad9709f7b5fd54adb0f379c9beb25a970a
Nolimet <nolimet@stamdata.nl>
2014-09-01 12:41:12 +0200

    and forgot a few little things

---
+++ Assets/Scripts/Inventory/InventoryStatics.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using System.Collections;
+namespace InvFrameWork
+{
+    public class InventoryStatics
+    {
+        public static string ResourceStartPath = "Inventory/Items/";
+    }
+}
