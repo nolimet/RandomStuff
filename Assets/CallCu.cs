b296adee91dfdfae471d84aba6d76be4a2b66867
Nolimet <nolimet@stamdata.nl>
2014-09-08 12:06:32 +0200

    some math stuff

--- Assets/CallCu.cs
+++ Assets/CallCu.cs
@@ -21,28 +21,68 @@ public class CallCu : MonoBehaviour {
 
     void Update()
     {
-        t += Time.deltaTime;
+        if(tps!=0)
+         t += Time.deltaTime;
 
-        if (t < 1 / tps)
+        if (tps!=0 && t < 1)
             return;
 
         Calculate();
         t = 0;
-        TotalFuel -= Consumption;
+        TotalFuel -= Consumption*tps;
     }
     void Calculate()
     {
-        float t = (TotalFuel / Consumption)/tps;
-        float s = Mathf.FloorToInt(t % 60);
-       // print(s);
-        float m = Mathf.FloorToInt(t / 60 % 60);
-        //print(m);
-        float h = Mathf.FloorToInt((t / 3600) % 24);
-      //  print(h);
-        float d = Mathf.FloorToInt((t /3600 /24));
-       // print(d);
 
-        time = " " + d + ":" + h + ":" + m + ":" + s;
+        float t = (TotalFuel / Consumption) / tps;
+        float fms = Mathf.FloorToInt((t * 1000) % 1000);
+
+        float fs = Mathf.FloorToInt(t % 60);
+        // print(s);
+        float fm = Mathf.FloorToInt(t / 60 % 60);
+        //print(m);
+        float fh = Mathf.FloorToInt((t / 3600) % 24);
+        //  print(h);
+        float d = Mathf.FloorToInt((t / 3600 / 24));
+        // print(d);
+
+        string ms;
+        string s;
+        string m;
+        string h;
+
+        if (fms < 10)
+            ms = "000" + fms;
+        else if (fms < 100)
+            ms = "00" + fms;
+        else
+            ms = "" + fms;
+
+        if (fs < 10)
+            s = "0" + fs;
+        else
+            s = "" + fs;
+
+        if (fm < 10)
+            m = "0" + fm;
+        else
+            m = "" + fm;
+
+        if (fh < 10)
+            h = "0" + fh;
+        else
+            h = "" + fh;
+
+
+        if (d == 0 && fh == 0 && fm == 0)
+            time = " " + s + ":" + ms;
+        else if (d == 0 && fh == 0)
+            time = " " + m + ":" + s + ":" + ms;
+        else if (d == 0)
+            time = " " + ":" + h + ":" + m + ":" + s + ":" + ms;
+        else
+            time = " " + d + ":" + h + ":" + m + ":" + s + ":" + ms;
+
     }
 
     void OnGUI()
---
+++ Assets/Scripts/misc/infinitelinev2.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class infinitelinev2 : MonoBehaviour {
+
+    [SerializeField]
+    float x = 150;
+    float yoff = 31.818f;
+
+    Vector2 l;
+    Vector2 AB = new Vector2(550,250);
+	// Use this for initialization
+	void Start () {
+        float dist = Mathf.Sqrt((Mathf.Pow(AB.x, 2) + Mathf.Pow(AB.y, 2)));
+        l = new Vector2(AB.x / dist, AB.y / dist);
+	}
+
+    Vector3 callp(float xd)
+    {
+        return new Vector3(x, l.y * x,0);
+    }
+
+    Vector3 call(float xd)
+    {
+        return new Vector3(-x, l.y * -x, 0);
+    }
+
+	// Update is called once per frame
+	void Update () {
+	    Debug.DrawLine(new Vector3(x,-10),new Vector3(x,10),Color.white,1f);
+        print(call(x));
+        print(callp(x));
+        Vector3 a = call(x);
+        Vector3 b = callp(x);
+        Debug.DrawLine(a, b,Color.red,1f);
+	}
+}
