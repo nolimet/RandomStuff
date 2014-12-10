94d332235a9776e768bd4684c1a0cf0ef9be1e1a
Nolimet <nolimet@stamdata.nl>
2014-12-10 11:16:30 +0100

    Trying to change UV's

---
+++ Assets/Scripts/Examples/VertexEditor.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class VertexEditor : MonoBehaviour {
+
+    void Start()
+    {
+        Mesh mesh = GetComponent<MeshFilter>().mesh;
+        Vector3[] vertices = mesh.vertices;
+        Vector2[] uvs = new Vector2[vertices.Length];
+        int i = 0;
+        while (i < uvs.Length)
+        {
+            uvs[i] = new Vector2(vertices[i].x, vertices[i].z);
+            i++;
+        }
+        mesh.uv = uvs;
+    }
+}
