d501ac30e3a9b40fb8df37405a138e6b64760229
Jesse Stam <nolimet@stamdata.nl>
2015-02-16 17:18:39 +0100

    added Item types and images

---
+++ Assets/Scripts/Inventory/V2/Item.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using System.Collections;
+namespace Invetory.V2
+{
+    public class Item : MonoBehaviour
+    {
+        public ItemType type;
+        [SerializeField]
+        private WeaponType _weapon;
+        public WeaponType weapon
+        {
+            get
+            {
+                if (type == ItemType.Weapon)
+                    return _weapon;
+                else
+                    return WeaponType.none;
+            }
+            set
+            {
+                this._weapon = value;
+            }
+        }
+        public Sprite icon;
+
+        void Start()
+        {
+            GetComponent<UnityEngine.UI.Image>().sprite = icon;
+            string tmp = name;
+            name = type.ToString() + " ";
+
+            if (weapon != WeaponType.none)
+                name += weapon.ToString() + " ";
+            name += tmp;
+        }
+    }
+
+    public enum ItemType
+    {
+        Armor,
+        Weapon,
+        Misc,
+        Key,
+        Potion
+    }
+
+    public enum WeaponType
+    {
+        none,
+        Sword,
+        Bow,
+        Shield
+    }
+}
