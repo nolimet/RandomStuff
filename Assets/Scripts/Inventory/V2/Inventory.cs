73d20da45d527a7533a0dc5027dd85e64a19bb47
Jesse Stam <nolimet@stamdata.nl>
2015-02-16 11:50:13 +0100

    Added new Inventory System using the new UI with dragable objects Moved old Inventory System into a folder in Invenotry/V1

---
+++ Assets/Scripts/Inventory/V2/Inventory.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.EventSystems;
+namespace Invetory.V2
+{
+    public class Inventory : MonoBehaviour, IHasChanged
+    {
+        [SerializeField]
+        Transform slots;
+        [SerializeField]
+        UnityEngine.UI.Text InventoryText;
+        // Use this for initialization
+        void Start()
+        {
+            onHasChanged();
+        }
+
+        public void onHasChanged()
+        {
+            System.Text.StringBuilder builder = new System.Text.StringBuilder();
+            builder.Append(" - ");
+            foreach (Transform slot in slots)
+            {
+                GameObject item = slot.GetComponent<ItemSlot>().item;
+                if(item){
+                    builder.Append(item.name);
+                    builder.Append(" - ");
+                }
+            }
+            InventoryText.text = builder.ToString();
+        }
+    }
+}
+namespace UnityEngine.EventSystems
+{
+    public interface IHasChanged : IEventSystemHandler
+    {
+        void onHasChanged();
+    }
+}
---
+++ Assets/Scripts/Inventory/V2/ItemDrag.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.EventSystems;
+namespace Invetory.V2
+{
+    public class ItemDrag : MonoBehaviour, IDragHandler, IBeginDragHandler, IEndDragHandler
+    {
+        public static GameObject selected;
+        public Vector3 StartPos;
+        public Transform StartParent;
+        Vector2 mouseOffSet;
+
+        public void OnBeginDrag(PointerEventData eventData)
+        {
+            mouseOffSet = eventData.position - (Vector2)transform.position;
+            selected = gameObject;
+            StartPos = transform.position;
+            StartParent = transform.parent;
+            GetComponent<CanvasGroup>().blocksRaycasts = false;
+        }
+
+        public void OnDrag(PointerEventData eventData)
+        {
+            transform.position = eventData.position - mouseOffSet;
+        }
+
+        public void OnEndDrag(PointerEventData eventData)
+        {
+            selected = null;
+            GetComponent<CanvasGroup>().blocksRaycasts = true;
+            if (transform.parent == StartParent)
+                transform.position = StartPos;
+
+        }
+    }
+}
